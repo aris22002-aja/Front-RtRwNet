364404e15362f38f921692a042c4d14717efe591
aris-22002-aja <aris.22002.sopir@gmail.com>
2026-04-30 15:56:14 +0700

    Upgrade Vite ke v8
    Upgrade Vite ke v8

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// const API_URL = 'http://localhost:3001/api';
-const API_URL = 'https://rt-rw.aris-22002-priyanto.workers.dev/api';
+const API_URL = 'http://localhost:3001/api';
 
 const api = axios.create({
   baseURL: API_URL,
